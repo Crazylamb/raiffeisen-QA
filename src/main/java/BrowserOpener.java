db48e2f62dc448297908443b04e6899c7ae718a3
Crazylamb <gabaranov@edu.hse.ru>
2021-11-14 02:57:45 +0300

    First task done

---
+++ src/main/java/BrowserOpener.java
@@ -0,0 +1,24 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class BrowserOpener {
+    public void OpenDrowser(){
+        WebDriver driver = null;
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("enable-automation");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-browser-side-navigation");
+        options.addArguments("--disable-gpu");
+        driver = new ChromeDriver(options);
+        driver.get("https://www.google.com/");
+        driver.close();
+    }
+}
---
+++ src/test/java/TestOpenBrowser.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class TestOpenBrowser {
+    @Test
+    public void TestOpen(){
+        BrowserOpener opener = new BrowserOpener();
+        opener.OpenDrowser();
+    }
+}

ffbc07be4a6839e37a6a44d90090154b91c6dffb
Crazylamb <gabaranov@edu.hse.ru>
2021-11-29 21:32:56 +0300

    Changed xpath to cssSelectors

--- src/test/java/Registration.java
+++ src/test/java/Registration.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.LocalDateTime;
@@ -32,47 +33,48 @@ public class Registration {
         driver.get("http://localhost/litecart/en/create_account");
 
         //registration
-        WebElement name = driver.findElement(By.xpath("//*[@id=\"create-account\"]/div/form/table/tbody/tr[2]/td[1]/input"));
+        WebElement name = driver.findElement(By.cssSelector("input[name=\"firstname\"]"));
         name.sendKeys("Name");
-        WebElement surname = driver.findElement(By.xpath("//*[@id=\"create-account\"]/div/form/table/tbody/tr[2]/td[2]/input"));
+        WebElement surname = driver.findElement(By.cssSelector("input[name=\"lastname\"]"));
         surname.sendKeys("Surname");
-        WebElement address = driver.findElement(By.xpath("//*[@id=\"create-account\"]/div/form/table/tbody/tr[3]/td[1]/input"));
+        WebElement address = driver.findElement(By.cssSelector("input[name=\"address1\"]"));
         address.sendKeys("New Street");
-        WebElement city = driver.findElement(By.xpath("//*[@id=\"create-account\"]/div/form/table/tbody/tr[4]/td[2]/input"));
+        WebElement city = driver.findElement(By.cssSelector("input[name=\"city\"]"));
         city.sendKeys("New City");
-        WebElement postcode = driver.findElement(By.xpath("//*[@id=\"create-account\"]/div/form/table/tbody/tr[4]/td[1]/input"));
+        WebElement postcode = driver.findElement(By.cssSelector("input[name=\"postcode\"]"));
         Random random = new Random();
         int post = random.nextInt(10000) + 10000;
         postcode.sendKeys(Integer.toString(post));
-        WebElement country =  driver.findElement(By.xpath("//*[@id=\"create-account\"]/div/form/table/tbody/tr[5]/td[1]/span[2]/span[1]/span"));
+        WebElement country =  driver.findElement(By.cssSelector("span[class=\"select2-selection__rendered\"]"));
         actions.moveToElement(country).click().perform();
-        WebElement countryInput = driver.findElement(By.xpath("/html/body/span/span/span[1]/input"));
+        WebElement countryInput = driver.findElement(By.cssSelector("input[class=\"select2-search__field\"]"));
         countryInput.sendKeys("United States" + Keys.ENTER);
+        WebElement state = driver.findElement(By.cssSelector("select[name=\"zone_code\"]"));
+        Select select = new Select(state);
+        select.selectByVisibleText("California");
         // Generating emails with hash function and local time to avoid repetitions
         email = Integer.toString(LocalDateTime.now().hashCode()) + "@gmail.com";
-        WebElement mail = driver.findElement( By.xpath("//*[@id=\"create-account\"]/div/form/table/tbody/tr[6]/td[1]/input"));
+        WebElement mail = driver.findElement(By.cssSelector("input[name=\"email\"]"));
         mail.sendKeys(email);
-        WebElement phoneNumber = driver.findElement(By.xpath("//*[@id=\"create-account\"]/div/form/table/tbody/tr[6]/td[2]/input"));
+        WebElement phoneNumber = driver.findElement(By.cssSelector("input[name=\"phone\"]"));
         phoneNumber.sendKeys("+12345678990");
-        WebElement passwordFirst = driver.findElement(By.xpath("//*[@id=\"create-account\"]/div/form/table/tbody/tr[8]/td[1]/input"));
+        WebElement passwordFirst = driver.findElement(By.cssSelector("input[name=\"password\"]"));
         passwordFirst.sendKeys(password);
-        WebElement passwordConfirm = driver.findElement(By.xpath("//*[@id=\"create-account\"]/div/form/table/tbody/tr[8]/td[2]/input"));
+        WebElement passwordConfirm = driver.findElement(By.cssSelector("input[name=\"confirmed_password\"]"));
         passwordConfirm.sendKeys(password);
-        WebElement register = driver.findElement(By.xpath("//*[@id=\"create-account\"]/div/form/table/tbody/tr[9]/td/button"));
+        WebElement register = driver.findElement(By.cssSelector("button[name=\"create_account\"]"));
         register.click();
 
         //Logout and login
-        String logout = driver.findElement(By.xpath("//*[@id=\"box-account\"]/div/ul/li[4]/a")).getAttribute("href");
-        driver.get(logout);
-        WebElement loginEmail = driver.findElement(By.xpath("//*[@id=\"box-account-login\"]/div/form/table/tbody/tr[1]/td/input"));
+        driver.findElement(By.linkText("Logout")).click();
+        WebElement loginEmail = driver.findElement(By.cssSelector("input[name=\"email\"]"));
         loginEmail.sendKeys(email);
-        WebElement loginPassword = driver.findElement(By.xpath("//*[@id=\"box-account-login\"]/div/form/table/tbody/tr[2]/td/input"));
+        WebElement loginPassword = driver.findElement(By.cssSelector("input[name=\"password\"]"));
         loginPassword.sendKeys(password);
-        driver.findElement(By.xpath("//*[@id=\"box-account-login\"]/div/form/table/tbody/tr[4]/td/span/button[1]")).click();
+        driver.findElement(By.cssSelector("button[name=\"login\"]")).click();
 
         //Final logout
-        logout = driver.findElement(By.xpath("//*[@id=\"box-account\"]/div/ul/li[4]/a")).getAttribute("href");
-        driver.get(logout);
+        driver.findElement(By.linkText("Logout")).click();
     }
 
     @After
